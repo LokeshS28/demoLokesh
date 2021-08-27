4bf4fdadc58ccf79b3be54b9bf90c0be82558cc1
lokeshS28 <lokesh.s@targetbay.com>
2021-08-27 02:11:54 -0700

    changes

--- component/layout.js
+++ component/layout.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "LOKESH S";
+const name = "LOKESH.S";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
